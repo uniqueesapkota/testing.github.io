bf0e8096e0c049aec8407be0064b132b740e9b97
Unique Sapkota <unique08mca2025@kcc.edu.np>
2025-06-18 16:49:59 +0545

    Update ProjectGallery.tsx

--- src/components/sections/ProjectGallery.tsx
+++ src/components/sections/ProjectGallery.tsx
@@ -21,7 +21,7 @@ const projects: Project[] = [
     id: "1",
     title: "Quick Commerce",
     description: "A full-featured online store with modern UI, secure payments, and robust inventory management. Built with Next.js, Tailwind CSS, and Stripe.",
-    imageUrl: "https://placehold.co/600x400.png",
+    imageUrl: "https://i.postimg.cc/RVYcPX4G/Screenshot-2025-06-1-imresizer.jpg",
     imageHint: "online store",
     liveLink: "https://commerce-next-cic5.vercel.app/",
     repoLink: "https://commerce-next-cic5.vercel.app/",
